ebb2824842c9779998d5b378b1582a250672d418
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2023-08-14 14:33:26 -0600

    Delete src directory

--- src/Fauna.java
+++
@@ -1,6 +0,0 @@
-public class Fauna {
-    public String species;
-    public String scientific;
-    public String continent;
-    public String fact;
-}
--- src/Flora.java
+++
@@ -1,6 +0,0 @@
-public class Flora {
-    public String species;
-    public String scientific;
-    public String continent;
-    public String fact;
-}
